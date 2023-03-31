8e88610cf854d662518cb5753eb77f820b578ffd
jako323 <jakokosinski@student.agh.edu.pl>
2023-03-31 17:52:58 +0200

    Added checkForWateringNeed draft function.

--- PlantClass.py
+++ PlantClass.py
@@ -12,6 +12,18 @@ class Plant:
 		self.pumpPin = pumpPin
 	
 
+	def checkForWateringNeed(self):
+		if (self.mode == 1):	# Manual mode	#TODO: Add conditions
+			return True
+		elif(self.mode == 2): #TODO: Add conditions
+			return True
+		elif(self.mode == 3):	#TODO: Add conditions
+			return True
+		else:
+			print("The plant", self.symbol, "is turned off or wrong mode was declared.")
+			return False
+
+
 	def watering(self):
 		print("The plant", self.symbol, "is now watered.")		
 		self.pumpOn()
--- main.py
+++ main.py
@@ -8,10 +8,21 @@ PLANT_B_PUMP_PIN = 16
 PLANT_C_PUMP_PIN = 20
 PLANT_D_PUMP_PIN = 21
 
+AUXILIARY_WATER_NOW_BUTTO_PIN = 25
+
+
 plantA = Plant('A', 0, 0.5, 0, 0, 0, PLANT_A_PUMP_PIN)
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(plantA.pumpPin, GPIO.OUT)
+GPIO.setup(AUXILIARY_WATER_NOW_BUTTO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-plantA.watering()
+while True:
+    if(plantA.checkForWateringNeed()):
+        plantA.watering()
+    sleep(1)
+
+
+GPIO.cleanup()
